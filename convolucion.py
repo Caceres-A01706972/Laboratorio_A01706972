83018efb1efedf220a54e37f95ce9d33f18d06bb
Ricardo Caceres <rcaceres1125@gmail.com>
2021-05-05 11:09:24 -0600

    Añade padding

--- convolucion.py
+++ convolucion.py
@@ -23,6 +23,13 @@ kernel_fila, kernel_columna = kernel.shape
 
 final = np.zeros(imagen.shape)
 
+pad_height = int((kernel_fila - 1)/2)
+pad_width = int((kernel_columna -1)/2)
+
+padded_image = np.zeros((imagen_fila + (2 * pad_height), imagen_columna + (2 * pad_width)))
+
+padded_image[pad_height:padded_image.shape[0] - pad_height, pad_width:padded_image.shape[1] - pad_width] = imagen
+
 for fila in range(imagen_fila):
     for columna in range(imagen_columna):
         final[fila, columna] = convolucion(imagen[fila:fila + kernel_fila,columna:columna + kernel_columna], kernel)
